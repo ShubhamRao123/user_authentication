d7e1961e9ce29813e79ed9b9d31a1f78e4c4b7f0
Shubham Rao <shubhamrao834@gmail.com>
2024-08-20 15:17:51 +0530

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-300 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-900">Delote</span>
+            <span className="text-slate-600"> Techonology</span>
+          </h1>
+        </Link>
+        <ul className=" flex gap-6">
+          <Link to="/">
+            <li className="hover:underline">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hover:underline">About</li>
+          </Link>
+          <Link to="sign-in">
+            <li className="hover:underline">SignIn</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
